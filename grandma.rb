c19fab58edd485c45f5cdd86d9aaf145896db18c
Heidi Hlavinka <hchlav@gmail.com>
2018-03-02 03:47:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!".upcase
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != "I LOVE YOU GRANDMA!".upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else phrase != "I LOVE YOU GRANDMA!".upcase
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
